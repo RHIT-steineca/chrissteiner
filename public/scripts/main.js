a07f7098408204a4a4006048b34a6d539ef28c77
RHIT-steineca <steineca@rose-hulman.edu>
2022-03-12 04:53:14 -0500

    updated resume and added tons of new stuff to site

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -27,4 +27,45 @@ $('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
         removeModal();
       }
     });
+});
+
+// Courtesy of https://codepen.io/annalarson/pen/AegVzq
+$(function(){  // $(document).ready shorthand
+  $('.initialload').animate({opacity:'1'}, 800);
+  $('.maininfo').animate({opacity:'1'}, 800);
+});
+$(document).ready(function() {
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+      /* Check the location of each desired element */
+      $('.hideme').each( function(i){
+          var bottom_of_object = $(this).position().top + ($(this).outerHeight() / 3);
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          /* If the object is completely visible in the window, fade it in */
+          if( bottom_of_window > bottom_of_object ){
+              $(this).animate({opacity:'1'}, 800);
+          }
+      }); 
   });
+});
+
+// Courtesy of w3
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("mainnav").style.fontSize = "2em";
+    document.getElementById("mainnav").style.padding = ".75em 0";
+  } else {
+    document.getElementById("mainnav").style.fontSize = "4em";
+    document.getElementById("mainnav").style.padding = ".75em 0";
+  }
+}
+
+// Courtesy of https://stackoverflow.com/questions/3664381/force-page-scroll-position-to-top-at-page-refresh-in-html
+$(window).on('beforeunload', function() {
+  $('body').hide();
+  $(window).scrollTop(0);
+});
